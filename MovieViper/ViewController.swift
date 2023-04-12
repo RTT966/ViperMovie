f0e99aa70e3bd0546008f583c12959b846564785
Rustam <toymastov@gmail.com>
2023-04-12 17:39:55 +0400

    Initial Commit

---
+++ MovieViper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieViper
+//
+//  Created by Рустам Т on 4/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
